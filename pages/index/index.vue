a09835844e34153c539f9b6e4f58a76af5aaba48
wangjianxing <wjx0825@gmail.com>
2020-05-05 15:03:26 +0800

    first add

---
+++ pages/index/index.vue
@@ -0,0 +1,68 @@
+<template>
+	<view>
+			<switch @change="typeChanged" />
+		<uni-list>
+			<uni-list-item v-for="(item, index) in topics" :key="item.id" :title="item.title" :thumb="item.user.avatar_url"
+			:note="item.user.name" :rightText="(item.excellent ? '💎\t' : '') + item.node_name" :show-badge="true" :badge-text="item.replies_count" @click="opentopic(index)"></uni-list-item>
+		</uni-list>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				topics: [],
+				topicType: 'last_actived',
+			}
+		},
+		onLoad:function(){
+			this.fetchTopics();
+		},
+		methods: {
+          opentopic(e) {
+			var item_id = this.topics[e].id
+			uni.navigateTo({
+				url: '../topic/topic?item_id=' + item_id,
+			});  
+		  },
+		  typeChanged(e) {
+			if (e.target.value == true) {
+			  this.topicType = 'excellent';
+			  uni.setNavigationBarTitle({
+			  	title: "💎 ruby-china 精华帖"
+			  });
+			}
+			else {
+				this.topicType = 'last_actived';
+				uni.setNavigationBarTitle({
+					title: "ruby-china 最新帖"
+				});
+			}
+			  
+			this.fetchTopics();
+		  },
+		  fetchTopics() {
+			  uni.request({
+			  	url: 'https://ruby-china.org/api/v3/topics.json',
+			  	method: 'GET',
+			  	data: { type: this.topicType, limit: 50 },
+			  	success: res => {
+			  				console.log(res);
+			  				this.topics = res.data.topics
+			  			},
+			  	fail: () => {},
+			  	complete: () => {}
+			  });
+		  }
+		}
+	}
+</script>
+
+<style>
+	.container {
+		padding: 20px;
+		font-size: 14px;
+		line-height: 24px;
+	}
+</style>
---
+++ pages/topic/topic.vue
@@ -0,0 +1,42 @@
+<template>
+	<view class="content">
+		<rich-text class="richText" :nodes="content"></rich-text>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: '',
+				content: '',
+			}
+		},
+		onLoad:function(e){
+			console.log(e);
+			uni.request({
+				url: 'https://ruby-china.org/api/v3/topics/' + e.item_id + '.json',
+				method: 'GET',
+				data: {},
+				success: res => {
+							console.log(res);
+							this.title = res.data.topic.title;
+							this.content = res.data.topic.body_html;
+						},
+				fail: () => {},
+				complete: () => {
+					uni.setNavigationBarTitle({
+						title: this.title,
+					})
+				}
+			});
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
